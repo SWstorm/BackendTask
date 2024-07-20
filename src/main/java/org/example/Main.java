d2ae0e0835cba3e4463648928ddc42d7022ba27d
SWStorm <prostitap@mail.ru>
2024-07-20 21:47:44 +0300

    Ex4.7

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,7 @@
 package org.example;
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello, Java!");
+        System.out.print("Hello, Java!");
 
         }
     }
