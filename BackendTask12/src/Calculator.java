8624e3741e960e84c71fd58203224b303a5cb208
SWstorm <168558393+SWstorm@users.noreply.github.com>
2024-07-24 13:26:41 +0300

    Add files via upload

---
+++ BackendTask12/src/Calculator.java
@@ -0,0 +1,9 @@
+public class Calculator {
+    Operation operation;
+    public Calculator(Operation operation) {
+        this.operation = operation;
+    }
+    public double calc(double a, double b) {
+        return operation.getResult(a, b);
+    }
+}
