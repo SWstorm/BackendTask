b8c183eb05f049d8ddbe21b637b5461e93bf63ae
SWStorm <prostitap@mail.ru>
2024-08-14 12:03:16 +0300

    Ex 5.10

---
+++ location/src/main/java/com/example/location/model/Geodata.java
@@ -0,0 +1,22 @@
+package com.example.location.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Geodata {
+    @Id @GeneratedValue
+    int id;
+    @NonNull private double longitude;
+    @NonNull private double latitude;
+    @NonNull private String name;
+}
